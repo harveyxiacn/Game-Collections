54d3e4f9969b899808f1eed6253c9d4ea67a08ab
Harvey Xia <harvey1991cn@gmail.com>
2015-02-19 17:40:39 -0400

    Update Keys_Tag.java
    Delete the keys.....

--- app/src/main/java/com/itgarage/harvey/gamecollections/amazon_product_advertising_api/Keys_Tag.java
+++ app/src/main/java/com/itgarage/harvey/gamecollections/amazon_product_advertising_api/Keys_Tag.java
@@ -5,10 +5,10 @@ package com.itgarage.harvey.gamecollections.amazon_product_advertising_api;
  */
 public class Keys_Tag {
     // change this for the keys, get from aws console, use root account to generate. IAM is unavailable.
-    public static final String AWS_ACCESS_KEY_ID = "AKIAJKQURIBSBUDWDJRA";
-    public static final String AWS_SECRET_KEY = "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";
+    public static final String AWS_ACCESS_KEY_ID = "";
+    public static final String AWS_SECRET_KEY = "";
     // change this for the associate tag which can use register links in
     // http://docs.aws.amazon.com/AWSECommerceService/latest/DG/becomingAssociate.html
     // for different locations
-    public static final String ASSOCIATE_TAG = "i0ded-20";
+    public static final String ASSOCIATE_TAG = "";
 }
